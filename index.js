84f774478ed91a647196f6ec8b037460e59371e3
Olga Tsybikova <tsy2701@gmail.com>
2022-04-14 19:11:53 +0200

    some minor changes

---
+++ index.js
@@ -0,0 +1,15 @@
+class SkylabArray {
+  constructor(
+    firstNumber,
+    secondNumber,
+    thirdNumber,
+    forthNumber,
+    fifthNumber
+  ) {
+    this.firstNumber = firstNumber;
+    this.secondNumber = secondNumber;
+    this.thirdNumber = thirdNumber;
+    this.forthNumber = forthNumber;
+    this.fifthNumber = fifthNumber;
+  }
+}
