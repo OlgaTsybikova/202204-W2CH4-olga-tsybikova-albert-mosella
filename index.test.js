ee0792746daba8345287d35fe8209b7407e6be8f
Olga Tsybikova <tsy2701@gmail.com>
2022-04-15 10:18:53 +0200

    Corrections of the code

--- index.test.js
+++ index.test.js
@@ -4,34 +4,37 @@ class SkylabArray {
   2;
   3;
 
-  constructor(long, height, width) {
-    this[0] = long;
-    this[0] = height;
-    this[0] = width;
+  constructor(length, height, width) {
+    this[0] = length;
+    this[1] = height;
+    this[2] = width;
   }
 }
+// function sum (...numbers) {
+// numbers[5, 6, 7]
+// }
 
 const getLength = (text) => {
   let longitud = 0;
-  for (let i = 0; i !== undefined; i++) {
-    if (text[i] !== undefined) {
+  for (let i = 0; i !== "undefined"; i++) {
+    if (text[i] !== "undefined") {
       longitud++;
     }
-    if (text[i] === undefined) {
+    if (text[i] === "undefined") {
       return longitud;
     }
   }
   return longitud;
 };
 
-const names = new SkylabArray(0, 5, 18);
-names.long = getLength(names);
+const numbers = new SkylabArray(0, 5, 18);
+numbers.length = getLength(numbers);
 
 describe("Given the function getLength", () => {
-  describe("When it receives 'Albert'", () => {
+  describe("When it receives numbers 0, 5, 18", () => {
     test("Then it should return 5", () => {
-      const text = { 0: "Maria", 1: "Albert", 2: "Alex", 3: "Leia", 4: "Max" };
-      const expectedResult = 3;
+      const text = { 0: 0, 1: 5, 2: 18 };
+      const expectedResult = 4;
 
       const result = getLength(text);
 
